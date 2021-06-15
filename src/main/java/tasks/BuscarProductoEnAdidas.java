0f2606c7e39e5ea7456665e0dcc1abb97d8e27cb
jefferson998 <pazjefferson11216@gmail.com>
2021-06-15 09:13:20 -0500

    nuevo cambio de interaction cambiar de ventana

--- src/main/java/tasks/BuscarProductoEnAdidas.java
+++ src/main/java/tasks/BuscarProductoEnAdidas.java
@@ -1,35 +1,31 @@
 package tasks;
 
 import interaction.Esperar;
+import interaction.changeToWindow;
 import net.serenitybdd.core.steps.Instrumented;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
-import net.serenitybdd.screenplay.actions.Switch;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 import org.openqa.selenium.Keys;
 import userInterface.AdidasUserInterface;
 import variablesGlobales.VariablesGlobales;
-
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 
 public class BuscarProductoEnAdidas implements Task{
-	public static String defaultWindows;
 	private String producto;
 	public BuscarProductoEnAdidas(String producto) {
 		this.producto = producto;
 	}
-
 	@Override
 	public <T extends Actor> void performAs(T actor) {
 		actor.attemptsTo(
 				Enter.theValue(producto).into(AdidasUserInterface.TXT_BUSCADOR_ADIDAS).thenHit(Keys.ENTER),
 				WaitUntil.the(AdidasUserInterface.BTN_PRODUCTO_ADIDAS.of(producto),isVisible()).forNoMoreThan(20).seconds(),
 				Click.on(AdidasUserInterface.BTN_PRODUCTO_ADIDAS.of(producto)),
-				Switch.toWindow(getNewWindowHandle(actor,defaultWindows)),
+				changeToWindow.on(),
 				Click.on(AdidasUserInterface.BTN_CLOSE_WINDOW_EMERGENTE),
 				Esperar.estosSegundos(30)
 		);
@@ -40,15 +36,5 @@ public class BuscarProductoEnAdidas implements Task{
 	public static BuscarProductoEnAdidas on(String producto) {
 		return Instrumented.instanceOf(BuscarProductoEnAdidas.class).withProperties(producto);
 	}
-
-	private static String getNewWindowHandle(Actor actor, String currentWindowHandle){
-		for (String windowHandle : BrowseTheWeb.as(actor).getDriver().getWindowHandles()) {
-			if (!windowHandle.equals(currentWindowHandle)) {
-				return windowHandle;
-			}
-		}
-		return null;
-	}
-
 
 }
--- src/main/java/userInterface/AdidasUserInterface.java
+++ src/main/java/userInterface/AdidasUserInterface.java
@@ -11,7 +11,7 @@ public class AdidasUserInterface {
 	public static final Target LBL_PRODUCTO_ADIDAS = Target.the("").locatedBy("//div[@class='product-description___2cJO2']/child::h1//span");
 	//categorias aleatorios
 	public static final Target BTN_PRODUCTO_ALEATORIO_ADIDAS=Target.the("").locatedBy("//div[@class='gl-product-card__assets']//a");
-	public static final Target LBL_PRODUCTO_ALEATORIO_ADIDAS=Target.the("").locatedBy("//div[@class='gl-product-card__details']//span");
+	public static final Target LBL_PRODUCTO_ALEATORIO_ADIDAS=Target.the("").locatedBy("//div[@class='grid-item___3rAkS']//div[@class='gl-product-card__details']/p[@data-auto-id='product-card-title']");
 	//categorias
 	public static final Target BTN_CATEGORY_HOMBRE=Target.the("").locatedBy("//div[@data-auto-id='glass-navigation-flyout']/a[contains(text(),'HOMBRE')]");
 	public static final Target BTN_CATEGORY_TENIS = Target.the("").locatedBy("//span[contains(text(),'Tenis')]//ancestor::button");
